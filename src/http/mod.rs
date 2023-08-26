d30c1495b6540460217f0b904d29f89784bcf675
Dave Miner <wtrispsn@gmail.com>
2023-08-26 12:38:18 -0400

    http unit tests

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -6,7 +6,9 @@ use serde::de::DeserializeOwned;
 use serde_derive::Deserialize;
 use thiserror::Error;
 
+/// Provides candlestick chart data.
 pub mod candlesticks;
+/// API implementation.
 pub mod v1;
 
 #[derive(Clone, Error, Debug, Deserialize)]
@@ -43,6 +45,7 @@ pub enum HttpVerb {
 pub const DEFAULT_SERVER_URL: &str = "https://api.sfox.com";
 
 impl Client {
+    /// Returns a new client with the default server URL.
     pub fn new() -> Result<Client, HttpError> {
         let server_url =
             env::var("SFOX_SERVER_URL").unwrap_or_else(|_| DEFAULT_SERVER_URL.to_string());
@@ -50,6 +53,7 @@ impl Client {
         build_server(server_url)
     }
 
+    /// Builds a new client with the given server URL; useful for testing.
     pub fn new_with_server_url(server_url: String) -> Result<Client, HttpError> {
         build_server(server_url)
     }
@@ -128,5 +132,62 @@ where
     match serde_json::from_str::<T>(&text) {
         Ok(payload) => Ok(payload),
         Err(e) => Err(HttpError::UnparseableResponse(e.to_string(), text)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tokio_tungstenite::tungstenite::http::Response;
+
+    use crate::http::{parse_response, Client, HttpError, DEFAULT_SERVER_URL};
+    use std::env;
+
+    #[test]
+    fn test_client_initialization() {
+        let _ = env::set_var("SFOX_AUTH_TOKEN", "abc123");
+        let client = Client::new().unwrap();
+
+        // Assert auth token was set correctly
+        assert_eq!(client.auth_token, "abc123");
+        assert_eq!(client.server_url, DEFAULT_SERVER_URL);
+    }
+
+    #[test]
+    fn test_client_initialization_with_url() {
+        let _ = env::set_var("SFOX_AUTH_TOKEN", "abc123");
+        let server_url = "http://localhost:4000".to_string();
+
+        let client = Client::new_with_server_url(server_url.clone()).unwrap();
+
+        assert_eq!(client.auth_token, "abc123");
+        assert_eq!(client.server_url, server_url);
+    }
+
+    #[tokio::test]
+    async fn test_parse_response_success() {
+        // Create mock response
+        let resp = Response::builder()
+            .status(200)
+            .body("\"response body\"")
+            .unwrap();
+
+        let parsed: String = parse_response(resp.into()).await.unwrap();
+        assert!(!parsed.is_empty())
+    }
+
+    #[tokio::test]
+    async fn test_parse_response_transport_error() {
+        // Create a failed response
+        let resp = Response::builder()
+            .status(400)
+            // Will not deserialize correctly without quotes
+            .body("response body")
+            .unwrap();
+
+        // Try to parse failed response
+        let result = parse_response::<String>(resp.into()).await;
+
+        // Assert error returned
+        assert!(matches!(result, Err(HttpError::TransportError(_))));
     }
 }
