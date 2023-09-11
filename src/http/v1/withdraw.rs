34a6b6742a224ece576dfe39fcf4ce3f11b31b56
Dave Miner <wtrispsn@gmail.com>
2023-09-10 21:12:17 -0400

    unit test for withdraw

--- src/http/v1/withdraw.rs
+++ src/http/v1/withdraw.rs
@@ -5,13 +5,16 @@ use serde::Deserialize;
 
 use super::super::{Client, HttpError, HttpVerb};
 
+static WITHDRAW_RESOURCE: &str = "user/withdraw";
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct Withdrawal {
     pub address: String,
+    pub atx_id: usize,
     pub amount: f64,
     pub currency: String,
-    #[serde(rename = "isWire")]
-    pub is_wire: bool,
+    pub success: bool,
+    pub tx_status: usize,
 }
 
 impl Client {
@@ -28,8 +31,47 @@ impl Client {
         params.insert("currency".to_string(), currency.to_string());
         params.insert("isWire".to_string(), is_wire.to_string());
 
-        let url = self.url_for_v1_resource("user/withdraw");
+        let url = self.url_for_v1_resource(WITHDRAW_RESOURCE);
 
         self.request(HttpVerb::Post, &url, Some(&params))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::util::server::{new_server_and_client, ApiMock};
+
+    const WITHDRAW_RESPONSE_BODY: &str = r#"
+        {
+            "success": true,
+            "id": "5pauoj52osolwphwnioqxx2zcekikm23x2hyq2rkotockiysng3y5k245q",
+            "atx_id": 1524562,
+            "tx_status": 1100,
+            "currency": "eth",
+            "amount": 0.1,
+            "address": "0x12345"
+        }
+    "#;
+
+    #[tokio::test]
+    async fn withdraw_test() {
+        let mock = ApiMock {
+            action: HttpVerb::Post,
+            body: WITHDRAW_RESPONSE_BODY.into(),
+            path: format!("/v1/{}", WITHDRAW_RESOURCE),
+            response_code: 200,
+        };
+
+        let (client, _server, mock_results) = new_server_and_client(vec![mock]).await;
+
+        let result = client.withdraw("0x00", 123.45, "btc", true).await;
+
+        assert!(result.is_ok());
+
+        for mock in mock_results {
+            mock.assert_async().await;
+        }
     }
 }
