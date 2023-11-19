32e5e62c96878d76fd496fb8ff35fd1db6e41e72
daveminer <wtrispsn@gmail.com>
2023-11-19 13:43:35 -0500

    websocket stuff

---
+++ src/websocket/market/order_book.rs
@@ -0,0 +1,39 @@
+use futures_util::SinkExt;
+use serde_derive::{Deserialize, Serialize};
+use tokio_tungstenite::tungstenite::Message;
+
+use crate::{http::HttpError, websocket::SFoxWs};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SubscribeMsg {
+    #[serde(rename = "type")]
+    pub msg_type: String,
+    pub feeds: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum OrderBookType {
+    #[serde(rename = "net")]
+    Net,
+    #[serde(rename = "sfox")]
+    SFox,
+}
+
+impl SFoxWs {
+    pub async fn subscribe(&mut self, feeds: Vec<String>) -> Result<(), HttpError> {
+        let msg = SubscribeMsg {
+            msg_type: "subscribe".to_string(),
+            feeds,
+        };
+
+        let msg = serde_json::to_string(&msg).map_err(|e| {
+            HttpError::InitializationError(format!("Could not serialize subscribe msg: {}", e))
+        })?;
+
+        self.write.send(Message::Text(msg)).await.map_err(|e| {
+            HttpError::InitializationError(format!("Could not send subscribe msg: {}", e))
+        })?;
+
+        Ok(())
+    }
+}
